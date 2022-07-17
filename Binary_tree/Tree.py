bd3edc0a673e621adefac5a1066818f64a671c86
Emam546 <emam54637@gmail.com>
2022-07-18 00:09:30 +0200

    Huffman implementation

---
+++ Binary_tree/Tree.py
@@ -0,0 +1,348 @@
+
+# Insert - O(log N) + O(N) = O(N)
+                    #O(N) in organizing in every insert
+# Find - O(log N)
+# Update - O(log N)
+# List all - O(N)
+
+class Basic_Tree():
+    def __init__(self, key,parent=None,left=None,right=None,value=None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.value=value
+        self.parent=parent
+
+    def __lt__(self, value):
+        if isinstance(value,Basic_Tree):
+            return self.key<value.key
+        return False
+    def __le__(self, value,):
+        if isinstance(value,Basic_Tree):
+            return self<value or self==value
+        return False
+    def __gt__(self, value):
+        if isinstance(value,Basic_Tree):
+            return self.key>value.key
+        return True
+    def  __ge__(self, value):
+        if isinstance(value,Basic_Tree):
+            return self>value or self==value
+        return True
+    def __eq__(self,value):
+        if isinstance(value,Basic_Tree):
+            return self.key==value.key
+        return False
+    def to_tuple(self):
+        if not isinstance(self,Basic_Tree):
+            return self
+        if not isinstance(self.left,Basic_Tree) and not isinstance(self.right,Basic_Tree):
+            return self.key
+        return BST.to_tuple(self.left),  self.key, BST.to_tuple(self.right)
+    
+    def __str__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+
+    def __repr__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+    @staticmethod
+    def parse_tuple(data,parent=None):
+        # print(data)
+        if isinstance(data, tuple) and len(data) == 3:
+            node = Basic_Tree(data[1],parent)
+            node.left = Basic_Tree.parse_tuple(data[0],node)
+            node.right = Basic_Tree.parse_tuple(data[2],node)
+        elif data is None:
+            node = None
+        else:
+            node = Basic_Tree(data,parent)
+        return node
+
+    def __bool__(self):
+        return True
+
+class BST(Basic_Tree):
+    def _height(self):
+        if not isinstance(self,Basic_Tree):
+            return 0
+        return 1 + max(BST._height(self.left), BST._height(self.right))
+    @property
+    def height(self):
+        
+        return self._height()
+    def __len__(self):
+        return self.size()
+    def __iter__(self):
+        return  (x for x in self.list_all())
+    def size(self):
+        if self is None:
+            return 0
+        return 1 + BST.size(self.left) + BST.size(self.right)
+
+    def traverse_in_order(self):
+        if self is None: 
+            return []
+        return (BST.traverse_in_order(self.left) + 
+                [self.key] + 
+                BST.traverse_in_order(self.right))
+    def traverse_in_postorder(self):
+        if self is None: 
+            return []
+        return (BST.traverse_in_postorder(self.left) + 
+                BST.traverse_in_postorder(self.right)+
+                [self.key])
+    def travers_in_preorder(self):
+        if self is None: 
+            return []
+        return ([self.key]+
+                BST.travers_in_preorder(self.left) + 
+                BST.travers_in_preorder(self.right)
+                )
+    @staticmethod
+    def display_keys(self:Basic_Tree, space='\t', level=0):
+        # If the node is empty
+        if self is None:
+            print(space*level + '∅')
+            return   
+
+        # If the node is a leaf 
+        if self.left is None and self.right is None:
+            print(space*level + str(self.key))
+            return
+
+        # If the node has children
+        BST.display_keys(self.right, space, level+1)
+        print(space*level + str(self.key))
+        BST.display_keys(self.left,space, level+1)    
+    
+    
+    def check_binary_tree_searching(self):
+        if self.right !=None:
+            return self.right.key<self.key and self.right.check_binary_tree_searching()
+        return True
+    def getmaxiumum_key(self):
+        if not isinstance(self,BST):
+            return 0
+        _list=[self,]
+        if isinstance(self.left,Basic_Tree):
+            _list.append(self.left.getmaxiumum_key())
+        if isinstance(self.right,Basic_Tree):
+            _list.append(self.right.getmaxiumum_key())
+        return max(_list)
+    def getminiumum_key(self):
+        if not isinstance(self,Basic_Tree):
+            return None
+        #we but lararge number to don't count it
+        _list=[self,]
+        if isinstance(self.left,Basic_Tree):
+            _list.append(self.left.getminiumum_key())
+        if isinstance(self.right,Basic_Tree):
+            _list.append(self.right.getminiumum_key())
+        return min(_list)
+    @staticmethod
+    def find(node: Basic_Tree, key):
+        if node is None:
+            return None
+        if key == node.key:
+            return node
+        if key < node.key:
+            return BST.find(node.left, key)
+        if key > node.key:
+            return BST.find(node.right, key)
+    def update(node, key, value):
+        target = BST.find(node, key)
+        if target is not None:
+            target.value = value
+    
+    def list_all(node):
+        if node is None:
+            return []
+        return BST.list_all(node.left) + [(node.key, node.value)] + BST.list_all(node.right)
+    @property
+    def width(self)->int:
+        return 2**(self.height-1)
+    def balance_bst(self):
+        return self.make_balanced_bst(self.list_all())
+    @staticmethod
+    def is_balanced(node):
+        if not isinstance(node,BST):
+            return True, 0
+        balanced_l, height_l = BST.is_balanced(node.left)
+        balanced_r, height_r = BST.is_balanced(node.right)
+        balanced = balanced_l and balanced_r and abs(height_l - height_r) <=1
+        height = 1 + max(height_l, height_r)
+        return balanced, height
+    def insert(node, key, value):
+        if node is None:
+            node = BST(key, value=value)
+        elif key < node:
+            node.left = BST.insert(node.left, key, value)
+            node.left.parent = node
+        elif key > node:
+            node.right = BST.insert(node.right, key, value)
+            node.right.parent = node
+        return node
+    def update(node, key, value):
+        target = BST.find(node, key)
+        if target is not None:
+            target.value = value
+    # Ensure that the left subtree is balanced.
+    # Ensure that the right subtree is balanced.
+    # Ensure that the difference between heights of left subtree and right subtree is not more than 1.
+    
+    def is_full(self):
+        #all the nodes has 2 or a zero children nodes
+        if self!=None:
+            if  BST.is_full(self.left) and BST.is_full(self.left):
+                return True
+            elif not BST.is_full(self.left) and not BST.is_full(self.left):
+                #check if they are both are empty
+                return True
+            else:
+                return False
+        else:
+            return False
+    def is_perfect(self):
+        return self.max_number_node()==self.size()
+    def is_complete(self):
+        #check if the tree is weel competed
+        if isinstance(self,Basic_Tree):
+            left_side=BST.is_complete(self.left)  
+            right_side=BST.is_complete(self.right)
+            if left_side ==True:
+                return True
+            elif left_side==None and right_side==None:
+                return True
+            else:
+                return False
+        else:
+            return None
+        
+
+    # @staticmethod
+    # def make_balanced_bst(data, lo=0, hi=None, parent=None):
+    #     if hi is None:
+    #         hi = len(data) - 1
+    #     if lo > hi:
+    #         return None
+        
+    #     mid = (lo + hi) // 2
+    #     key, value = data[mid]
+
+    #     root = BST(key, value)
+    #     root.parent = parent
+    #     root.left = make_balanced_bst(data, lo, mid-1, root)
+    #     root.right = make_balanced_bst(data, mid+1, hi, root)
+        
+    #     return root
+
+    #not acurate_way    
+    def is_binary_search(self):
+        #checking if its empty or not
+        #not accurate
+        if self is None:
+            return True
+        state=True
+        if self.left!=None:
+            if self.left>self:
+                state=False
+            elif not BST.is_binary_search(self.left):
+                state=False
+        if state and self.right!=None:
+            #if they are any repeating in data
+            if self.right<=self:
+                state=False
+            elif not BST.is_binary_search(self.left):
+                state=False
+        return state
+    #overwrite this previous method
+
+    def is_binary_search(self):
+        #print("___new method")
+        if self  >  BST.getminiumum_key(self.right):
+            return False
+        if self  <=  BST.getmaxiumum_key(self.right):
+            return False   
+        return True
+
+    def parse_tuple(data,parent=None):
+        # print(data)
+        if isinstance(data, tuple) and len(data) == 3:
+            node = BST(data[1],parent)
+            node.left = BST.parse_tuple(data[0],node)
+            node.right = BST.parse_tuple(data[2],node)
+        elif data is None:
+            node = None
+        else:
+            node = BST(data,parent)
+        return node
+
+    @staticmethod
+    def parse_tuple(data,parent=None):
+        # print(data)
+        if isinstance(data, tuple):
+            if len(data) == 3:
+                node = BST(data[1],parent)
+                node.left = BST.parse_tuple(data[0],node)
+                node.right = BST.parse_tuple(data[2],node)
+            elif len(data)==2:
+                node = BST(data[0],parent,value=data[1])
+            else:
+                raise "undefined tuple"
+        elif data is None:
+            node = None
+        return node
+
+    def delete(self,value):
+        node=self.find(value)!=None
+        if node==None:return
+        sticked_node=None
+        if node.left==None and node.right==None:
+            sticked_node=None
+        elif self.left==None and self.right!=None:
+            sticked_node=self.right
+        elif self.left!=None and self.right==None:
+            sticked_node=self.left
+        else:
+            #if they are both are emty
+            sticked_node=self.right.getminiumum_key()
+            #in case of repeating same key we will not apply recusion
+
+            #theres is no need to check whethere it is right or left .
+            sticked_node.parent.right=sticked_node.right
+        if self.parent!=None:
+            if self==self.parent.right:
+                self.parent.right=sticked_node 
+            else:
+                self.parent.left=sticked_node
+        elif sticked_node is None:
+            raise "you can't make the tree empty"
+
+
+if __name__=="__main__":
+    example_tuple=(((-1,-1,0),1,2),3,(4,5,(7,8,9)))
+    un_balanced=((0,1,2),3,(4,5,(7,8,9)))
+    un_binary_search=((2,1,0),3,(4,5,6))
+    un_is_complete=((None,1,(1.5,0,2.5)),3,(4,5,6))
+    empty_list=(2)
+    for _tuple in [example_tuple,un_balanced,un_binary_search,un_is_complete,empty_list]:
+        tree=BST.parse_tuple(_tuple)
+        #print("is_full :", tree.is_full())
+        # print("is_complete :",tree.is_complete())
+        print("is_balanced :",tree.is_balanced())
+        print("is_perfect :",tree.is_perfect())
+        print("is_binary_search :",tree.is_binary_search())
+
+        print("size",tree.size())
+        print("height",tree.height())
+        print("width",tree.width())
+        print("max value",tree.getmaxiumum_key())
+        print("min value",tree.getminiumum_key())
+        print("__________________________")
+
+# insert(tree, biraj.username, biraj)
+# insert(tree, sonaksh.username, sonaksh)
+# insert(tree, aakash.username, aakash)
+# insert(tree, hemanth.username, hemanth)
+# insert(tree, siddhant.username, siddhant)
+# insert(tree, vishal.username, siddhant)
---
+++ huffman.py
@@ -0,0 +1,122 @@
+from Binary_tree.Tree import BST, Basic_Tree
+from typing import *
+import os,json
+class HuffmanTree(Basic_Tree):
+    @property
+    def repreBits(self):
+        assert self.parent,f"there is no parent to the code {self.parent} {self}"
+        return self._parent(self)
+    @property
+    def repreByte(self):
+        bits=self.repreBits
+        return "00000000"[len(bits):]+bits
+    @staticmethod
+    def _parent(node:Basic_Tree,orgbyte=""):
+        if not node.parent:
+            return orgbyte
+        if id(node.parent.left)==id(node):
+            return HuffmanTree._parent(node.parent,"1"+orgbyte)
+        if id(node.parent.right)==id(node):
+            return HuffmanTree._parent(node.parent,"0"+orgbyte)
+      
+            
+        
+        
+def get_codec(file: str):
+    propabilites = dict()
+    def add(byte: bytes):
+        if ord(byte) in propabilites:
+            propabilites[ord(byte)] += 1
+        else:
+            propabilites[ord(byte)]=1
+    with open(file, "rb") as f:
+        byte=f.read(1)
+        while byte:
+            add(byte)
+            byte=f.read(1)
+    size=os.stat(file).st_size
+    bytesprop=list(map(lambda item:HuffmanTree(item[1]/size,value=item[0]),propabilites.items()))
+    props=bytesprop.copy()
+    while len(props)!=1:
+        props.sort(reverse=True)
+        left,right=props[-2:]  
+        key=left.key+right.key
+        newNode=Basic_Tree(key,None,left,right)
+        for bst in [left,right]:
+            bst.parent=newNode
+            props.pop(-1)
+        props.append(newNode)
+    codecs=dict()
+    entropy=0
+    for prop in bytesprop:
+        codecs[prop.value]=bits=prop.repreBits
+        entropy+=len(bits)*prop.key
+    return codecs
+def transfer_file(file,resultfile,codecs:dict=None):
+    assert resultfile!=file,"the orginal file and resultfile must be no same"
+    encoding:dict=codecs if codecs else get_codec(file)
+    with open(resultfile,"wb") as resultf:
+        with open(file,"rb") as f:
+            byte=f.read(1)
+            code=""
+            while byte:
+                while byte and len(code)<8:
+                    code+=encoding[ord(byte)]
+                    byte=f.read(1)
+                
+                byte_code=int(code[:8],2)
+                resultf.write(bytes([byte_code]))
+                code=code[8:]
+            while code:
+                print("LEAST",code)
+                byte_code=int(code[:8],2)
+                result=bin(byte_code)[2:]
+           
+                resultf.write(bytes([byte_code]))
+                code=code[8:]
+    with open(f"{resultfile}.keys","w") as f:
+        json.dump(encoding,f)
+    result_size=os.stat(resultfile).st_size
+    org_size=os.stat(file).st_size
+    print(f"the File redced by {round(100-(result_size/org_size)*100,2)}%")
+def retransfer(file,result_file):
+    assert file!=result_file,"the result file must be another file"
+    codecsFile=f"{file}.keys"
+    #assert not os.path.exists(codecsFile),f"the keys of the {codecsFile} doesn't exists"
+    with open(codecsFile,"r") as f:
+        codec=dict({(value,str(key)) for key,value in json.load(f).items()})
+    def search(byte: bytes,last_code):
+        byte=bin(ord(byte))[2:]
+        binary=last_code+"00000000"[len(byte):]+byte
+        result_bytes=[]
+        start=0
+        for end in range(1,len(binary)):
+            if binary[start:end] in codec:
+                result_bytes.append(
+                    int(codec[binary[start:end]])
+                    )
+                start=end
+        return binary[start:],bytes(result_bytes)
+    with open(result_file,"wb") as resultFile:
+        with open(file,"rb") as f:
+            byte=f.read(1)
+            last_code=""
+            while byte:
+                last_code,result_bytes=search(byte,last_code)
+                resultFile.write(result_bytes)
+                byte=f.read(1)
+            if last_code:
+                print(last_code)
+                last_code,result_bytes=search(bytes([0]),last_code)
+                print(last_code)
+    print("DECODING FINISNHED PROPBABLY")        
+
+def __main():
+    transfer_file(r"./test_text.txt","./text_resultfile.txt")
+    # #__test()
+    retransfer("./text_resultfile.txt","final_result.txt")
+if __name__ == "__main__":
+    __test()
+    
+    
+    
